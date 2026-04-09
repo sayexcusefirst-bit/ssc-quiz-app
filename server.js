1fb767f61e43b765d3ea22a4648c19aff6105500
sayexcusefirst-bit <sayexcusefirst@gmail.com>
2026-04-09 17:01:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/questions", async (req, res) => {
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
-    model: "llama3-70b-8192",
+  model: "llama3-8b-8192",
     messages: [
       {
         role: "user",
